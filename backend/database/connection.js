b269371bc96bbd7924823d58c219692161880878
massimopallera <pallera.massimo@gmail.com>
2024-12-10 13:18:20 +0100

    created query to show films

---
+++ backend/database/connection.js
@@ -0,0 +1,28 @@
+import sql from "mysql2"
+import dotenv from "dotenv"
+import chalk from "chalk";
+
+const connection = sql.createConnection({
+	host: dotenv.config().parsed.DB_HOST,
+	port: dotenv.config().parsed.DB_PORT,
+	user: dotenv.config().parsed.DB_USER,	
+	password: dotenv.config().parsed.DB_PASSWORD,
+	database: dotenv.config().parsed.DATABASE
+})
+
+connection.connect((err) => {
+	console.log('Connected to Database');
+	console.log(`+------------------------------------------------------------+
+| Database Connection Information                            |
++------------------------------------------------------------+
+| Host: ${dotenv.config().parsed.DB_HOST}                      			     |
+| Port: ${dotenv.config().parsed.DB_PORT}                      			     |
+| User: ${dotenv.config().parsed.DB_USER}						     |
+| Database: ${dotenv.config().parsed.DATABASE}				     |
++------------------------------------------------------------+
+| Connection Status: ${(err) ? (chalk.green('Connected')+"\t\t\t\t    ") : (chalk.red('Not Connected')+"\t\t\t    ")} |
++------------------------------------------------------------+
+`)
+})
+
+export default connection
