0714501fda5c76037f59b3aec00d04cd384ca682
massimopallera <pallera.massimo@gmail.com>
2024-12-10 15:42:17 +0100

    added routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 const server = express()
-import connection from './database/connection.js'
+// import connection from './database/connection.js'
+import moviesRouter from './routes/movies.js'
 
 
 server.use(express.json())
@@ -10,40 +11,4 @@ const PORT = process.env.PORT || 3000 || 3001 || 3002
 
 server.listen(PORT, ()=> console.log(`Server listening on ${HOST}:${PORT}`))
 
-server.get('/', (req,res) => {
-
-    const sql = `SELECT * FROM movies`
-
-    connection.query(sql, (err, results) => {
-        if (err) res.status(err.code).send(err.message)
-        if (!results[0]) res.status(404).send('404 Not Found')
-        res.status(200).json({'results': results})
-    })
-})
-
-//show one film
-server.get('/:id', (req,res) => {
-
-    const sql = `SELECT r.*
-    FROM reviews AS r
-    INNER JOIN movies AS m ON m.id = r.movie_id
-    WHERE m.id = ?`
-
-    let element
-
-    const getElement = `SELECT * FROM movies WHERE id = ?`
-
-    connection.query(getElement, [req.params.id], (err,results) => {
-        element = results
-    })
-
-    connection.query(sql, [req.params.id] ,(err, results) => {
-        if (err) res.status(err.code).send(err.message)
-        if (!results[0]) res.status(404).send('404 Not Found')
-        res.status(200).json({
-            'results': results, 
-            'movie':element
-        })        
-    })
-    
-})
+server.use('/', moviesRouter)
---
+++ backend/routes/movies.js
@@ -0,0 +1,45 @@
+import express from "express";
+import connection from "../database/connection.js";
+
+const router = express.Router()
+
+router.get('/', (req,res) => {
+
+    const sql = `SELECT * FROM movies`
+
+    connection.query(sql, (err, results) => {
+        if (err) res.status(err.code).send(err.message)
+        if (!results[0]) res.status(404).send('404 Not Found')
+        res.status(200).json({'results': results})
+    })
+})
+
+//show one film
+router.get('/:id', (req,res) => {
+
+    const sql = `SELECT r.*
+    FROM reviews AS r
+    INNER JOIN movies AS m ON m.id = r.movie_id
+    WHERE m.id = ?`
+
+    let element
+
+    const getElement = `SELECT * FROM movies WHERE id = ?`
+
+    connection.query(getElement, [req.params.id], (err,results) => {
+        element = results
+    })
+
+    connection.query(sql, [req.params.id] ,(err, results) => {
+        if (err) res.status(err.code).send(err.message)
+        if (!results[0]) res.status(404).send('404 Not Found')
+        res.status(200).json({
+            'results': results, 
+            'movie':element
+        })        
+    })
+    
+})
+
+
+export default router
